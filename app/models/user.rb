2f684e264bab790d42823391d90e3c968f1883f5
Mika Lindell <mika.lindell82@gmail.com>
2013-03-18 18:48:35 +0200

    User model: name and email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,19 @@
 
 class User < ActiveRecord::Base
 	attr_accessible :name, :email
+
+	#Make lowercase emails before save for system compatibility
+	before_save { |user| user.email = email.downcase }
+
+	validates :name, 
+						presence: true, 
+						length: { maximum: 50 }
+	
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, 
+						presence: true, 
+						format: { with: VALID_EMAIL_REGEX },
+						uniqueness: { case_sensitive: false }
+						#REMEMBER: UNIQUENESS IS NOT FOOLPROOF!
+						#SEE TUTO SECTION 6.2.5
 end
